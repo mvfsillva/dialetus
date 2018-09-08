386a89996fa8f194d63f724107d7a1a19a04898f
Marcus Silva <mvfsillva@gmail.com>
2018-09-08 14:00:49 -0300

    feature/theme (#5)
    * add transition animate
    
    * add config styled-components with next
    
    * add new rules for the theme
    
    * fix rollback styled-components version

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class Mydocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <html lang="pt-br">
+        <Head>
+          <title>Dialetos Brasileiros</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
