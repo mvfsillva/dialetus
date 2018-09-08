1575ced0fda1cd72d42e4525070086a15baf005b
Marcus Silva <mvfsillva@gmail.com>
2018-09-08 13:59:18 -0300

    add new rules for the theme

--- theme/index.js
+++ theme/index.js
@@ -1,37 +1,55 @@
 'use strict'
 
+import { darken, lighten } from 'polished'
+
 // @media screen and (min-width: 36em) - 576px
 // @media screen and (min-width: 48em) - 768px
 // @media screen and (min-width: 62em) - 992px
 // @media screen and (min-width: 75em) - 1.200px
 const breakpoints = ['36em', '48em', '62em', '75em']
+const black = '#000000'
+const gray = '#7e7e7e'
 
 const colors = {
-  primary: {
-    default: '#6800c3',
-    fallback: '#a55cff',
-    text: '#fff',
+  white: '#ffffff',
+
+  black: Object.assign(black, {
+    lighter: lighten('0.2', black),
+    light: lighten('0.1', black),
+    dark: darken('0.025', black),
+    darker: darken('0.1', black),
+  }),
+
+  gray: Object.assign(gray, {
+    lighter: lighten('0.2', gray),
+    light: lighten('0.1', gray),
+    dark: darken('0.025', gray),
+    darker: darken('0.1', gray),
+  }),
+
+  shadow: Object.assign('0 2px 6px rgba(0, 0, 0, .16)', {
+    small: '0 1px 1px rgba(0, 0, 0, .16)',
+    medium: '0 8px 10px rgba(0, 0, 0, .16)',
+    large: '0 24px 32px rgba(0, 0, 0, .16)',
+  }),
+}
+
+const typography = {
+  hero: {
+    fontSize: '44px',
+    lineHeight: '48px',
+    fontWeight: '700',
+  },
+  h100: {
+    fontSize: '26px',
+    lineHeight: '32px',
+    fontWeight: '500',
+  },
+  body: {
+    fontSize: '20px',
+    lineHeight: '28px',
+    fontWeight: '300',
   },
 }
 
-const fontFamilies = {
-  sansSerif: 'Helvetica, sans-serif',
-}
-
-const fontWeights = {
-  sansSerif: {
-    normal: 300,
-    bold: 500,
-  },
-  serif: {
-    normal: 'normal',
-    bold: 'bold',
-  },
-}
-
-export default {
-  breakpoints,
-  colors,
-  fontFamilies,
-  fontWeights,
-}
+export { breakpoints, colors, typography }
