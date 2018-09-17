bde8c0e428d2f85321f8b7ba50c9ee018319a89f
Marcus Silva <mvfsillva@gmail.com>
2018-09-16 21:50:11 -0300

    add spec

--- components/spinner/index.spec.js
+++ components/spinner/index.spec.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { mountWithTheme } from 'config/test/enzyme.theme'
+
 import theme from 'theme'
-import Card from '.'
+import Spinner from '.'
 
 import 'jest-styled-components'
 
 const mount = mountWithTheme(theme)
 
-describe('[Component: Card]', () => {
+describe('[Component: Spinner]', () => {
   it('mounts component', () => {
-    mount(<Card />)
-    mount(<Card  size="small"/>)
-    mount(<Card  size="medium"/>)
-    mount(<Card  size="large"/>)
+    mount(<Spinner />)
   })
 })
