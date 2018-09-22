f6d3e4db75c885103381088434bc7ad45e3ce5f5
Marcus Silva <mvfsillva@gmail.com>
2018-09-21 23:33:41 -0300

    add brazil maps component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-'use strict'
-
 import React from 'react'
+import styled from 'styled-components'
 
 import Header from 'components/header'
 import ListCard from 'container/list-card'
+import Brazil from 'components/brazil'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  align-items: center;
+  margin: 50px;
+`
 
 const Home = () => (
-  <div>
-    <Header headline="Dialetus" dialect="Baianes" />
-    <br />
-    <ListCard />
-  </div>
+  <Container>
+    <Brazil />
+  </Container>
 )
 
 export default Home
