a2459564bcf87ac1bb75785c42e63dd37f1d57d1
Marcus Silva <mvfsillva@gmail.com>
2018-09-30 20:39:07 -0300

    add config next

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const pkg = require('./package')
+
+require('dotenv').config()
+
+module.exports = {
+  poweredByHeader: false,
+  generateBuildId: async () => pkg.version,
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+  }
+}
