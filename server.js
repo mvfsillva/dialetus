7b7df9ce36b2864610d78f7e4288b4bdbd22eb06
Bu Kinoshita <bukinoshita@gmail.com>
2018-10-06 22:55:07 -0300

    fix dynamic route (#24)

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const next = require('next')
 const routes = require('./routes')
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const handler = routes.getRequestHander(app)
+const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => createServer(handler).listen(3000))
