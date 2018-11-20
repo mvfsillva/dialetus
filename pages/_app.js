098ac8b5562fe2daf76cf1f349b064987f67e3f3
Marcus Silva <mvfsillva@gmail.com>
2018-11-20 20:50:59 -0200

    fix improvement theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import theme from '../theme'
-import injectGlobalStyles from '../theme/global-style'
+
+import theme from 'theme/default'
 
 class MyApp extends App {
-  static async getInitialProps({ Component, _, ctx }) {
+  static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
+
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+      pageProps = await Component.getInitialProps(ctx);
     }
+
     return { pageProps }
   }
 
   render() {
     const { Component, pageProps } = this.props
-
-    injectGlobalStyles()
-
     return (
-      <ThemeProvider theme={theme}>
-        <Container>
+      <Container>
+        <ThemeProvider theme={theme}>
           <Component {...pageProps} />
-        </Container>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Container>
     )
   }
 }
