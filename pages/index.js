1c1408b02c80a6f11116852c06de27a6b0ccc496
mvfsilva <mvfsillva@gmail.com>
2019-02-18 18:07:12 -0300

    patch: clean home page file

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
-import React, { PureComponent, Fragment } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react'
 
-import Header from 'components/header'
-import Brazil from 'components/brazil'
-import Footer from 'components/footer'
-
-import Section from 'layouts/section'
-
-import { Router } from '../routes'
-
-const Main = styled.main`
-  display: flex;
-  justify-content: center;
-
-  padding-top: 56px;
-  padding-bottom: 56px;
-`
-
-class Home extends PureComponent {
-  getDialect = region => Router.pushRoute('dialect', { slug: region })
-
+class Home extends Component {
   render () {
-    return (
-      <Section>
-        <Header headline="Dialetos Brasileiros" />
-        <Main>
-          <Brazil handleSelected={this.getDialect} />
-        </Main>
-      </Section>
-    )
+    return <h1>Dialetus</h1>
   }
 }
 
