a78a44cd4b609dbca8ff5b660d4eefd1427aec60
mvfsilva <mvfsillva@gmail.com>
2019-02-20 17:05:16 -0300

    patch: add more specs

--- components/header/index.spec.js
+++ components/header/index.spec.js
@@ -9,5 +9,16 @@ const wrap = (props = {}) => shallow(<Header {...props} />)
 describe('[Component: Header]', () => {
   it('should mount <Header /> component', () => {
     mount(<Header />)
+    mount(<Header headline="Brazilian Dialects" />)
+  })
+
+  it('should verify if the props exists', () => {
+    const wrapper = wrap({ headline: 'Dialetus' })
+    expect(wrapper.props().children).toBeTruthy()
+  })
+
+  it('should register called one time change', () => {
+    const wrapper = wrap({ headline: 'Dialetus' })
+    expect(wrapper.contains('Dialetus')).toBe(true)
   })
 })
