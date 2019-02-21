a4003264f6496dc3c1d3fa14637840266088928a
Marcus Silva <mvfsillva@gmail.com>
2019-02-21 02:36:10 -0300

    minor: layout page (#42)
    * patch: remove section styled-component =/
    
    * minor: add flex-center styled-component
    
    * minor: add full-container styled-component
    
    * minor: add main styled-component
    
    * minor: add default template layout
    
    * minor: add default template layout
    
    * patch: update home page

---
+++ styles/main.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Main = styled.main`
+  width: 100%;
+  max-width: 1200px;
+  padding: ${({theme}) => theme.spacing.xxxLarge};
+  margin-left: auto;
+  margin-right: auto;
+`
+export default Main
