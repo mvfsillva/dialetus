ba098f30aa14a977099eee69b1dfbd97406e2f44
mvfsilva <mvfsillva@gmail.com>
2019-02-21 01:23:06 -0300

    minor: add footer component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 import Header from 'components/header'
+import Footer from 'components/footer'
 
 class Home extends Component {
   render () {
-    return <Header />
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    )
   }
 }
 
