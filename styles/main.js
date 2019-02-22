f3f1459dbf29294ca896ef64ad30e9f5cc4929ed
mvfsilva <mvfsillva@gmail.com>
2019-02-22 02:40:26 -0300

    patch: update max-width of main

--- styles/main.js
+++ styles/main.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Main = styled.main`
   width: 100%;
-  max-width: 1200px;
+  max-width: 1400px;
   padding: ${({theme}) => theme.spacing.xxxLarge};
   margin-left: auto;
   margin-right: auto;
