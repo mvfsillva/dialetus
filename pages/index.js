8ad07ef62c40645519462a4bd89abb5c0fe7981e
Marcus Silva <mvfsillva@gmail.com>
2019-02-21 23:42:00 -0300

    patch: improvement layout (#47)
    * patch: remove white line
    
    * patch: add headline text
    
    * patch: add components on page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react'
 
 import Page from '../layouts/default'
+import Card from 'components/card'
+import PageTitle from 'components/page-title'
+import Search from 'components/search'
 
 class Home extends Component {
   render () {
     return (
       <Page>
-          <p>
-            Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est.
-            Sapien in monti palavris qui num significa nadis i pareci latim. Mauris nec dolor in eros commodo tempor.
-            Aenean aliquam molestie leo, vitae iaculis nisl. Viva Forevis aptent taciti sociosqu ad litora torquent.
-          </p>
-          <p>
-            Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Suco de cevadiss deixa as pessoas mais interessantis.
-            Detraxit consequat et quo num tendi nada. Cevadis im ampola pa arma uma pindureta.
-          </p>
-          <p>
-            Nullam volutpat risus nec leo commodo, ut interdum diam laoreet.
-            Sed non consequat odio. Quem num gosta di mé, boa gentis num é.
-            Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!
-            Aenean aliquam molestie leo, vitae iaculis nisl.
-          </p>
+        <PageTitle title="Baianês" />
+        <Search />
+        <Card />
       </Page>
     )
   }
