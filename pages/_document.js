a9879e423f61cf04c87f231d798d60a18d878329
mvfsilva <mvfsillva@gmail.com>
2019-03-01 01:00:25 -0300

    patch: adjustment on meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <meta name="theme-color" content={props => props.theme.palette.primary} />
+          <meta name="theme-color" content="#282035" />
           <meta name="description" content={pkg.description} />
           <meta name="keywords" content={pkg.keywords} />
           <meta name="twitter:card" content="summary_large_image" />
