0de75d53b304f453106522b18ed7f2237c977758
Alison Souza <alisonmsouza@yahoo.com.br>
2019-03-05 18:58:37 -0300

    add security http headers

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ const routes = require('./routes')
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
 
-app.prepare().then(() => createServer(handler).listen(3000))
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handler(req, res)
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('Content-Security-Policy', 'self');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.setHeader('X-Content-Type-Option', 'nosniff');
+  }).listen(3000)
+})
