388d18f69606324494d7bed881912bb7f4728cc5
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-06 18:02:36 -0300

    patch: add next.config target serverless

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const pkg = require('./package')
 require('dotenv').config()
 
 module.exports = {
+  target: 'serveless',
   poweredByHeader: false,
   generateBuildId: async () => pkg.version,
   publicRuntimeConfig: {
-    API_URL: process.env.API_URL,
+    API_URL: process.env.API_URL
   }
 }
