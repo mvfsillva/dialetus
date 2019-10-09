498b5914ba206cd0ed2cddcb40cfc2f2d3c727ae
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2019-10-08 22:45:50 -0300

    update to pt-br the lang attribute

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,7 @@ class MyDocument extends Document {
 
   render () {
     return (
-      <html lang="pt">
+      <html lang="pt-br">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
