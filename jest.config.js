0db6a1ce6bce42a0a68fc725435f8f4864643e1c
Marcus Silva <mvfsillva@gmail.com>
2020-07-05 20:26:51 -0300

    chore: update root config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  ...require('./config/test/jest-common'),
-  collectCoverageFrom: ['**/src/"**/*.+(js|json|ts|tsx)"'],
-  projects: ['./config/test/jest.client.js'],
-  preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.jest.json',
-    },
-  },
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverageFrom: ['**/*.ts(x)?', '!/**/*.stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/config/test-setup.ts']
 }
