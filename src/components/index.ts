d8a89010f5f795ca07fd937f1d7daf2420127c3c
Marcus Silva <mvfsillva@gmail.com>
2021-02-21 14:53:09 -0300

    feat: export all components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Header from './header'
+import Footer from './footer'
+import Card from './card'
+import Input from './input'
+import Regions from './regions'
+
+export { Header, Footer, Card, Input, Regions }
