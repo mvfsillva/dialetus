d7310e9123e8384c0444890c050fc2fb98a30716
Marcus Silva <mvfsillva@gmail.com>
2021-03-01 17:50:20 -0300

    refactor: change interface to type

--- styled-components.d.ts
+++ styled-components.d.ts
@@ -3,5 +3,5 @@ import theme from 'styles/theme'
 type Theme = typeof theme
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
+  export type DefaultTheme = Theme
 }
